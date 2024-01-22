0bc9a5396c31ac7101786655d20cc73aff69e0ad
Björn <bjorn.christensson@osynlig.se>
2024-01-22 16:04:03 +0100

    comment on Storybook Tailwind issue

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,13 @@
 import type { Preview } from '@storybook/react';
 
+/**
+ * Targeting this file works but it's not ideal.
+ * Ideally we would target some Tailwind css output,
+ * but I'm not sure how to do that. This solution is a bit janky since
+ * it seems to rely on the next dev server running (not CI friendly and sub-optimal DX).
+ */
 import '../.next/static/css/app/layout.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
