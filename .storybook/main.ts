f9460ef16d9972c4c030fee54e9968cbfc5fe4b0
Björn <bjorn.christensson@osynlig.se>
2024-01-22 14:57:00 +0100

    Try fix tailwind in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,51 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+
+import tailwindConfig from '../tailwind.config.ts';
+
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    if (!config || !config.module || !config.module.rules) return config;
+
+    console.log(tailwindConfig);
+
+
+    // Load Tailwind CSS as a PostCSS plugin
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require('tailwindcss')(tailwindConfig),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
+
 };
 export default config;
