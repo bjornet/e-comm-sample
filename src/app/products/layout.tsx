8f9d97bb01afe0578096fbd1c5e38d49db186259
Björn <bjorn.christensson@osynlig.se>
2024-01-23 00:23:14 +0100

    clenup

--- src/app/products/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-
-import { ProductsProvider } from '$context/products/productsContext';
-
-const ProductsLayout: FC<PropsWithChildren<{}>> = ({ children }) => (
-  <ProductsProvider>{children}</ProductsProvider>
-);
-export default ProductsLayout;
