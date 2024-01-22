3aa76bd60a2ae06c93827d5532ce01b3c9d17ac4
Björn <bjorn.christensson@osynlig.se>
2024-01-22 15:55:57 +0100

    Simplify storybook tailwind setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import path from 'path';
-
-import tailwindConfig from '../tailwind.config.ts';
-
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
   framework: {
@@ -19,33 +16,5 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    if (!config || !config.module || !config.module.rules) return config;
-
-    console.log(tailwindConfig);
-
-
-    // Load Tailwind CSS as a PostCSS plugin
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: [
-                require('tailwindcss')(tailwindConfig),
-                require('autoprefixer'),
-              ],
-            },
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    });
-
-    return config;
-  },
-
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,16 @@
 import type { Preview } from '@storybook/react';
 
-import '../src/styles/globals.css';
-
+import '../.next/static/css/app/layout.css';
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: '#fafafa' },
+        { name: 'dark', value: '#064E3B' },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
